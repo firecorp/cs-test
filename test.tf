192349df431c16647a4dded430eeaf7298866a49
ahsanabdul99 <ahsanabdul99@gmail.com>
2022-02-28 14:02:11 -0600

    Update test.tf

--- test.tf
+++ test.tf
@@ -6,6 +6,6 @@ resource "azurerm_postgresql_server" "example_ssl_enforcement_enabled" {
     administrator_login_password        = "H@Sh1CoR3!"
     sku_name                            = "GP_Gen5_4"
     storage_mb                          = 640000
-    version                             = "9.6"
+    version                             = "9.7"
     ssl_enforcement_enabled      = false
 }
